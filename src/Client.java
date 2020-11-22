cbbc0eb165decb7002f0571ea505bac67cb2b524
Jesse Guardado <guardadojesse@yahoo.com>
2020-11-22 09:59:46 -0800

    commented code

--- src/Client.java
+++ src/Client.java
@@ -4,41 +4,48 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.util.Scanner;
 
+// creating a class for client
 public class Client {
+    // declaring static variable for buffer_size
     static final int BUFFER_SIZE = 1000;
 
     public static void main(String[] args) {
         Scanner reader = null;
         DatagramSocket socket = null;
 
+        /* 
+        * Try statement will test for errors while executing 
+        * catch statment will handle the errors 
+        */
         try {
             reader = new Scanner(System.in);
             String port = null;
             String ip = null;
-
+            // checking for error handing if invalid input
             if (args.length == 2) {
                 port = args[0];
                 ip = args[1];
-
+              
                 if (!validatePort(port) || !validateIp(ip)) {
                     throw new Exception("Pass valid arguments! Try: java Client <port> <ip address>");
                 }
             } else {
                 while (true) {
+                    // if condition is met, proceed for port number
                     System.out.print("Enter the port number: ");
                     port = reader.nextLine();
-
+                    // checking if port number is valid
                     if (!validatePort(port)) {
                         System.out.println("Enter a valid port!");
                         continue;
                     }
                     break;
                 }
-
+                // prompt for ip address
                 while (true) {
                     System.out.print("Enter the ip address: ");
                     ip = reader.nextLine();
-
+                    // error handling message
                     if (!validateIp(ip)) {
                         System.out.println("Enter a valid ip!");
                         continue;
@@ -56,6 +63,7 @@ public class Client {
                 String msg = reader.nextLine();
 
                 InetAddress host = InetAddress.getByName(ip);
+                // get msg in bytes and length
                 DatagramPacket request = new DatagramPacket(
                     msg.getBytes(),
                     msg.getBytes().length,
@@ -64,8 +72,10 @@ public class Client {
 
                 socket.send(request);
 
+                // buffer for incoming data
                 byte[] buffer = new byte[BUFFER_SIZE];
                 DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+                // receiving the datagram packet
                 socket.receive(response);
                 displayResponseInfo(response);
             }
@@ -82,7 +92,7 @@ public class Client {
             }
         }
     }
-
+    // display response information for client: IP and Size
     private static void displayResponseInfo(DatagramPacket res) {
         System.out.println("\n**********************");
         System.out.println("* Data from Server   *");
@@ -91,7 +101,7 @@ public class Client {
             + "\nIP: " + res.getAddress()
             + "\nSize: " + res.getLength());
     }
-
+    // check if ip meeting conditional requirements
     private static boolean validateIp(String ip) {
         String ipRegex = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\."
             + "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\."
@@ -100,6 +110,7 @@ public class Client {
         return ip.matches(ipRegex) || ip.equals("localhost");
     }
 
+    // check if port meets conditional requirements
     private static boolean validatePort(String port) {
         String portRegex = "(^[1-9]{4}$)";
         return port.matches(portRegex);
--- src/Server.java
+++ src/Server.java
@@ -3,19 +3,29 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
+
+// creatting a class for server
 public class Server {
+
+    // Declaring static variables 
     static final int PORT = 6789;
     static final int BUFFER_SIZE = 1000;
 
     public static void main(String[] args) {
         DatagramSocket socket = null;
 
+        /* 
+        * Try statement will test for errors while executing 
+        * catch statment will handle the errors 
+        */
         try {
+            // initalizing an object
             socket = new DatagramSocket(PORT);
             byte[] buffer = new byte[BUFFER_SIZE];
 
             System.out.println("Server executed. Running on port " + PORT);
 
+            // 
             while (true) {
                 System.out.println("\nWaiting for a packet...");
                 DatagramPacket received = new DatagramPacket(buffer, BUFFER_SIZE);
@@ -23,6 +33,7 @@ public class Server {
 
                 displayRequestInfo(received);
 
+                // Getters for datagram packet
                 DatagramPacket response = new DatagramPacket(
                     received.getData(),
                     received.getLength(),
@@ -33,7 +44,7 @@ public class Server {
                 socket.send(response);
                 buffer = new byte[BUFFER_SIZE];
             }
-
+          // error handling 
         } catch (SocketException e) {
             System.out.println(Server.class.getName() + " : SOCKET : " + e.getMessage());
         } catch (IOException e) {
@@ -44,7 +55,7 @@ public class Server {
             }
         }
     }
-
+    // displaying data from client: IP and Size
     private static void displayRequestInfo(DatagramPacket req) {
         System.out.println("**********************");
         System.out.println("* Data from Client   *");
